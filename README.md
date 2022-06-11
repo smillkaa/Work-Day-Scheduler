6ec11f7f0f75b61b96e4e4edf17b3ebdbda3e191
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-06-10 20:13:39 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+### URL: https://smillkaa.github.io/Work-Day-Scheduler/
+---
+## Description
+A simple calendar application that allows a user to save events for each hour of the day.
+
+---
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- jQuery
+---
+## Usage
+- This calendar displays the work hours from 9 AM to 5 PM.
+- Current day and date is diplayed on top of the page.
+- Each time block color represents the past (grey), present(red), and future(green).
+- Each time block is an input field and users can save their schedule clicking on the button.
+- Saved schedule is stored in the local storage.
+- This calendar is desktop and mobile responsive.
+---
+
+### Front Page
+
+![img](assets/images/img.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,41 @@ $(document).ready(function () {
     // displays current date and time at the top of the page
     $("#currentDay").text(DateTime.now().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY))
 
+    // setting time to 24hr format
     var currentHour = DateTime.now().toFormat("H")
-    var timeBlock = $(".text-area");
+    var timeBlock = $(".text-area")
 
     // for each element with the class text-area, checks what time it is and color codes accordingly
-    timeBlock.each(function() {
-        
-        var hour = parseInt($(this).attr("id").split("hour")[1]) // i don't understand this
-        if(hour == currentHour){
-            $(this).addClass("present")
-        } else if (currentHour > hour){
-            $(this).addClass("past")
-        } else {
-            $(this).addClass("future")
-        }
+    function currentTime() {
+        timeBlock.each(function() {
+            
+            var hour = parseInt($(this).parent().attr("id").split("hour")[1])
+            if(hour == currentHour){
+                $(this).addClass("present")
+            } else if (currentHour > hour){
+                $(this).addClass("past")
+            } else {
+                $(this).addClass("future")
+            }
+        })
+    }
+
+
+    // local storage
+    $(".saveBtn").on("click", function () {
+        $(this).addClass("active")
+        // get the user text input and hour
+        var text = $(this).siblings(".text-area").val()
+        var time = $(this).parent().attr("id")
+
+        // Save text in local storage
+        localStorage.setItem(time, text)
     })
 
-}) // document get ready function ends
+    // Get item from local storage if any
+    for(var i=0; i < 9; i++) {
+        $(`#hour${i+9} .text-area`).val(localStorage.getItem(`hour${i+9}`))
+    }
 
-// local storage
-// var btnEl = $("textarea[type='submit']")
-// btnEl.on("click", function(event){
-//     event.preventDefault()
-    
-// })
+    currentTime()
+})
